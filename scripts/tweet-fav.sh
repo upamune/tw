b06997bdfaacade9c52c8cea1a93fca12835b264
upamune <jajkeqos@gmail.com>
2015-08-08 22:11:06 +0900

    Modify peco script tl cnt

--- scripts/tweet-fav.sh
+++ scripts/tweet-fav.sh
@@ -1,3 +1,3 @@
 function peco-favorite-tweet() {
-tw tl --with-id | peco --prompt "TWEET> " | awk '{print $NF}' | tw fav --pipe
+tw tl 100 --with-id | peco --prompt "TWEET> " | awk '{print $NF}' | tw fav --pipe
 }
--- scripts/tweet-rt.sh
+++ scripts/tweet-rt.sh
@@ -1,3 +1,3 @@
 function peco-retweet-tweet() {
-tw tl --with-id | peco --prompt "TWEET> " | awk '{print $NF}' | tw rt --pipe
+tw tl 100 --with-id | peco --prompt "TWEET> " | awk '{print $NF}' | tw rt --pipe
 }
